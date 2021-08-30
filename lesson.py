2139316218f0f7724d4fc5a8a31e2a2f72d0bb89
Alexander_Sovetnikov <sovetnikov05@icloud.com>
2021-08-30 22:45:15 +0700

    FOR loop

--- lesson.py
+++ lesson.py
@@ -9,3 +9,12 @@ print(cars)
 print(cars[-1])
 
 
+for names in cars:
+    print(names)
+
+    print("This is " + names)
+
+ints = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for single in ints:
+    print(single ** 5)
